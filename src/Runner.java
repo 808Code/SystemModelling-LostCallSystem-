653f78f7e934498243d3dd942a36649f5cc129d6
808Code <dikshant.shah2k@gmail.com>
2022-01-04 11:11:16 +0545

    console properly layed out

--- src/Runner.java
+++ src/Runner.java
@@ -8,11 +8,21 @@ public class Runner {
 //            System.out.println(Caller.callers[1]);
             call.checkProgressCalls();
             call.checkArrivalCalls();
-            System.out.println(Utility.clock++);
+            System.out.println("Current Clock="+Utility.clock++);
             Utility.pause();
             if(Utility.clock==3000){
                 break;
             }
+            System.out.println("The Progress Queue:");
+            for (Progress progress:call.getCallsInProgress()) {
+                System.out.println(progress);
+            }
+            System.out.println("The Arrival Queue:");
+            for (Arrival arrival:call.getCallsToArrive()) {
+                System.out.println(arrival);
+            }
+
+            System.out.println("-------------------------------------------------");
         }
         System.out.println("Thankyou For Simulating our Lost Call System.");
     }
